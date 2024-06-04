afae91aaf54a40af5593c4e2cb5ad42283a87a7e
L Howard <lynda.howard@nhs.net>
2024-06-04 12:22:49 +0100

    rename_row fixed

--- R/rename_row.R
+++ R/rename_row.R
@@ -10,12 +10,12 @@
 
 rename_row <- function(df, row_number){
   # Extract the named row containing the new column names)
-  new_col_names <- df[row_number,-c(1:3)]
+  new_col_names <- df[{{row_number}},-c(1:3)]
 
   # Rename the columns (excluding the first three columns)
-  df <- df %>%
-    dplyr::rename_at(dplyr::vars(-table_name, -folder_name, -index_no), ~ as.character(new_col_names))
+  df <- df  |> dplyr::rename_at(dplyr::vars(-table_name, -folder_name, -index_no), ~ as.character(new_col_names))
   # Remove the first row_number rows
-  df_new <- df[-c(1:row_number), ]
+  #df_new <- df[-c(1:row_number), ]
+  df_new <- df |> dplyr::slice(-(1:{{row_number}}))
   return(df_new)
 }
