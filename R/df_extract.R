49c7d48a30a8fd99910ead4fc9acf72cf89e103f
L Howard <lynda.howard@nhs.net>
2024-06-05 15:35:38 +0100

    missing {{}}

--- R/df_extract.R
+++ R/df_extract.R
@@ -7,4 +7,4 @@
 #' @export
 #'
 #' @examples split_tables(data_example("sample_download_file.xlsx"))|> df_extract("Workforce Overview")
-df_extract <- function(df, which_table){df |> dplyr::filter(table_name == which_table) |> tidyr::pivot_wider(id_cols = c(table_name, folder_name, index_no), names_from = col_names) }
+df_extract <- function(df, which_table){df |> dplyr::filter(table_name == {{which_table}}) |> tidyr::pivot_wider(id_cols = c(table_name, folder_name, index_no), names_from = col_names) }
